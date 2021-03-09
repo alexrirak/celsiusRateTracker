ddbf1f06b4e2b08fed95b561d3b8a2dc3d90c4d2
Alex Rirak <alexrirak@yahoo.com>
2021-03-08 21:13:15 -0500

    email alerts part 1

--- app.py
+++ app.py
@@ -3,32 +3,34 @@ import requests
 import json
 import mysql.connector
 from apscheduler.schedulers.background import BackgroundScheduler
+import smtplib
+import ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
-from db import db_host, db_user, db_password, db_database
+from db import db_host, db_user, db_password, db_database, email_username, email_smtp_server, email_password
 
 app = Flask(__name__)
 sched = BackgroundScheduler(daemon=True)
 
-CELSIUS_ENVIRONMENT = "prod"
+CELSIUS_ENVIRONMENT = "staging"
 CELSIUS_API_URL = "https://wallet-api.celsius.network/util/interest/rates" if CELSIUS_ENVIRONMENT == "prod" else "https://wallet-api.staging.celsius.network/util/interest/rates"
 # queries defined at the very bottom
 FETCH_COIN_DATA_QUERY = ""
 INSERT_COIN_DATA_QUERY = ""
 GET_LATEST_ROW_BY_COIN_QUERY = ""
 GET_METADATA_BY_COIN_QUERY = ""
-
 INSERT_COIN_METADATA_QUERY = ""
 UPDATE_COIN_METADATA_QUERY = ""
 
 
 @app.route('/')
 def main():
-    interestRates = get_celsius_rates()['interestRates']
     return render_template('main.html', env=CELSIUS_ENVIRONMENT)
 
 
 # Fetches data from the db on all the coins
-# returns data fro current rate and prior rate
+# returns data for current rate and prior rate
 @app.route('/fetchCoinData')
 def fetch_coin_data():
     mydb = get_db_connection()
@@ -47,15 +49,19 @@ def fetch_coin_data():
 
 
 # Given a coin and a rate checks if the rate is new and if so persists it to the DB
-def insert_coin_rate(coin, rate):
+# Returns true if it inserts a row, false if the rate was the same
+def insert_coin_rate(coin: str, rate):
     mydb = get_db_connection()
 
     mycursor = mydb.cursor()
+
+    rate_updated = False
 
     mycursor.execute(GET_LATEST_ROW_BY_COIN_QUERY.format(coin))
 
     existing_row = mycursor.fetchone()
 
+    # Check if the row exists
     if existing_row is not None:
         savedRate = float(existing_row[2])
 
@@ -63,12 +69,15 @@ def insert_coin_rate(coin, rate):
         if savedRate != float(rate):
             mycursor.execute(INSERT_COIN_DATA_QUERY, (coin, rate))
             mydb.commit()
+            rate_updated = True
     else:
         mycursor.execute(INSERT_COIN_DATA_QUERY, (coin, rate))
         mydb.commit()
+        rate_updated = True
 
     mycursor.close()
     mydb.close()
+    return rate_updated
 
 
 # Given the name, symbol and image url of a coin, check if meta data exists in the DB
@@ -97,17 +106,74 @@ def update_coin_metadata(name, symbol, image_url):
 @sched.scheduled_job('interval', id='refreshCoinData', hours=6)
 def process_coin_rates():
     interestRates = get_celsius_rates()['interestRates']
+    changed_rates = []
 
     # for each coin, check if we need to update rate data and meta data
     for coin in interestRates:
-        insert_coin_rate(coin["coin"], coin["rate"])
+        coin_rate_changed = insert_coin_rate(coin["coin"], coin["rate"])
         update_coin_metadata(coin["currency"]["name"], coin["currency"]["short"], coin["currency"]["image_url"])
+        if coin_rate_changed:
+            changed_rates.append(coin["coin"])
+
+    # If there are rates which changed trigger an async email alert
+    if len(changed_rates) > 0:
+        sched.add_job(send_out_email_alerts, None, [changed_rates])
 
     # since this method is http accessible need to ensure we always return a http status code
     return ('Success', 200)
 
 
-# Fetches the latest con rates from the celsius api
+# TODO: remove this method
+@app.route('/triggerEmail')
+def trigger_email():
+    send_out_email_alerts(["BTC", "ETH"])
+    #send_out_email_alerts(["ALX"])
+    return ('Success', 200)
+
+
+# Send out email alerts for the changed rates
+def send_out_email_alerts(changed_rates: list[str]):
+    coin_data = get_coin_change_data(changed_rates)
+    subscribers = get_subscribed_emails(changed_rates)
+
+    if len(subscribers) < 1:
+        return
+
+    for sub in subscribers:
+        send_rate_change_notification(sub["email"])
+        # TODO: send the coin data as well
+
+
+def get_subscribed_emails(changed_rates: list[str]):
+    mydb = get_db_connection()
+
+    mycursor = mydb.cursor()
+
+    # format the list as string but strip the brackets
+    mycursor.execute(GET_SUBSCRIBED_EMAILS.format(str(changed_rates).strip("[]")))
+
+    # this maps the column names onto the result set so that there is no guessing
+    columns = mycursor.description
+    result = [{columns[index][0]: column for index, column in enumerate(value)} for value in mycursor.fetchall()]
+
+    mydb.close()
+
+    return result
+
+
+# Based on a list of coins, fetches the relevant change data from the api
+def get_coin_change_data(changed_rates: list[str]):
+    coin_data = json.loads(fetch_coin_data())
+    coin_change_data = {}
+
+    for coin_record in coin_data:
+        if coin_record["coin"] in changed_rates:
+            coin_change_data[coin_record["coin"]] = coin_record
+
+    return coin_change_data
+
+
+# Fetches the latest coin rates from the celsius api
 def get_celsius_rates():
     response = requests.get(CELSIUS_API_URL)
 
@@ -135,6 +201,31 @@ def get_db_connection():
     )
 
 
+# sends an email to the specified email with the given subject and body
+def send_email(to_email: str, subject: str, body: str):
+
+    message = MIMEMultipart("alternative")
+    message["Subject"] = subject
+    message["From"] = email_username
+    message["To"] = to_email
+
+    message.attach(MIMEText(body, "html"))
+
+    # Create secure connection with server and send email
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(email_smtp_server, 465, context=context) as server:
+        server.login(email_username, email_password)
+        server.sendmail(
+            email_username, to_email, message.as_string()
+        )
+
+
+# Sends an email using the rate change template to the given email
+def send_rate_change_notification(to_email: str):
+    body = render_template('email.html')
+    send_email(to_email, "Celsius Rate Change", body)
+
+
 if __name__ == '__main__':
     app.run()
 
@@ -145,6 +236,7 @@ GET_LATEST_ROW_BY_COIN_QUERY = get_string_from_file('sql/getLatestRowByCoin.sql'
 GET_METADATA_BY_COIN_QUERY = get_string_from_file('sql/getMetadataByCoin.sql')
 INSERT_COIN_METADATA_QUERY = get_string_from_file('sql/insertCoinMetadata.sql')
 UPDATE_COIN_METADATA_QUERY = get_string_from_file('sql/updateCoinMetadata.sql')
+GET_SUBSCRIBED_EMAILS = get_string_from_file('sql/getSubscribedEmails.sql')
 
 # Start the scheduler
 sched.start()
